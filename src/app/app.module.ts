94f9978c8d9f9c31610cde22e37ddc1849376b55
gusrodsanz <gusrodsanz@gmail.com>
2021-08-28 12:34:20 +0200

    add post-list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import {MatInputModule} from '@angular/material/input';
 
 import { AppComponent } from './app.component';
-import { PostCreateComponent } from './post/post-create/post-create.component';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { HeaderComponent } from './header/header.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostCreateComponent,
-    HeaderComponent
+    HeaderComponent,
+    PostListComponent
   ],
   imports: [
     BrowserModule,
