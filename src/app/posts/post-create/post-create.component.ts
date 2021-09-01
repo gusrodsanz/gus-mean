ef2d35059b7b69faf0cdf29ea32ad77a46cff4c4
gusrodsanz <gusrodsanz@gmail.com>
2021-09-01 16:48:44 +0200

    add mime-type for image

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -37,8 +37,8 @@ export class PostCreateComponent implements OnInit {
 
       image: new FormControl(null, {
         validators: [Validators.required],
-        asyncValidators: [mimeType],
-      }),
+        asyncValidators: [mimeType]
+      })
     });
     this.route.paramMap.subscribe((paramMap: ParamMap) => {
       if (paramMap.has('postId')) {
