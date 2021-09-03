05e1ee16ac0f8c6ced6133adfe515295615ebe87
gusrodsanz <gusrodsanz@gmail.com>
2021-09-03 19:12:04 +0200

    add image store

--- backend/models/post.js
+++ backend/models/post.js
@@ -1,11 +1,9 @@
 const mongoose = require("mongoose");
 
 const postSchema = mongoose.Schema({
-  id: {type: String},
-  title: {type: String, required: true},
-  content: {type: String, required: true}
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  imagePath: { type: String, required: true }
 });
 
-
-module.exports = mongoose.model('Post', postSchema);
-
+module.exports = mongoose.model("Post", postSchema);
--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -17,31 +17,25 @@ export class PostListComponent implements OnInit, OnDestroy {
   // ];
   posts: Post[] = [];
   isLoading = false;
-
   private postsSub: Subscription;
 
   constructor(public postsService: PostsService) {}
 
   ngOnInit() {
     this.isLoading = true;
-
-
-    // this.posts =
     this.postsService.getPosts();
     this.postsSub = this.postsService.getPostUpdateListener()
       .subscribe((posts: Post[]) => {
         this.isLoading = false;
-
         this.posts = posts;
       });
+  }
+
+  onDelete(postId: string) {
+    this.postsService.deletePost(postId);
   }
 
   ngOnDestroy() {
     this.postsSub.unsubscribe();
   }
-
-  onDelete(postId: string){
-    this.postsService.deletePost(postId);
-  }
-
 }
