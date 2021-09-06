60fe9614955416e9490b39aeae29030b9acf8669
gusrodsanz <gusrodsanz@gmail.com>
2021-09-06 15:22:51 +0200

    add login screen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from "@angular/core";
-import { ReactiveFormsModule } from "@angular/forms";
+import { ReactiveFormsModule, FormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
 
 import { MatInputModule } from '@angular/material/input';
@@ -32,6 +32,7 @@ import { LoginComponent } from './auth/login/login.component';
   imports: [
     BrowserModule,
     ReactiveFormsModule,
+    FormsModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
