76622f0acfb5594125c6c54a56d2808a6d1e95b7
gusrodsanz <gusrodsanz@gmail.com>
2021-09-12 14:17:29 +0200

    add userIsAuthenticated  for post lists ts

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,53 +1,62 @@
-import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
-import { Subject } from "rxjs";
-import { map } from "rxjs/operators";
-import { Router } from "@angular/router";
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Subject } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
-import { AuthData } from "./auth-data.model";
+import { AuthData } from './auth-data.model';
 
-@Injectable({ providedIn: "root" })
+@Injectable({ providedIn: 'root' })
 export class AuthService {
+  private isAuthenticated = false;
   private token: string;
   private authStatusListener = new Subject<boolean>();
 
   constructor(private http: HttpClient, private router: Router) {}
 
-  getToken(){
+  getToken() {
     return this.token;
   }
 
-  getAuthStatusListener(){
+  getIsAuth () {
+    return this.isAuthenticated;
+  }
+
+  getAuthStatusListener() {
     return this.authStatusListener.asObservable();
   }
 
-  createUser(email: string, password: string){
+  createUser(email: string, password: string) {
     const authdata: AuthData = {
       email: email,
-      password: password
+      password: password,
     };
 
-    this.http.post("http://localhost:3000/api/user/signup", authdata)
-    .subscribe(res => {
-      console.log(res);
-    });
+    this.http
+      .post('http://localhost:3000/api/user/signup', authdata)
+      .subscribe((res) => {
+        console.log(res);
+      });
   }
 
-  loginUser(email: string, password: string){
+  loginUser(email: string, password: string) {
     const authdata: AuthData = {
       email: email,
-      password: password
+      password: password,
     };
 
-    this.http.post<{token: string}>("http://localhost:3000/api/user/login", authdata)
-    .subscribe(res => {
-      console.log(res);
-      const token = res.token;
-      this.token = token;
-      this.authStatusListener.next(true);
-    });
+    this.http
+      .post<{ token: string }>('http://localhost:3000/api/user/login', authdata)
+      .subscribe((res) => {
+        console.log(res);
+        const token = res.token;
+        this.token = token;
+
+        if (token) {
+          this.isAuthenticated = true;
+          this.authStatusListener.next(true);
+        }
+
+      });
   }
-
-
-
 }
